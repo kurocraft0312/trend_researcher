e61cd2d589c2ff572e72f118707900f3a1cfaedc
kurocraft0312 <kurocraft0312@gmail.com>
2020-05-02 16:30:52 +0900

    APIテスト

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,2 +1,19 @@
 "use strict";
 
+//XMLHttpRequestオブジェクトの作成
+var request = new XMLHttpRequest();
+
+// URLを開く
+request.open('GET','https://ghapi.huchen.dev/repositories');
+
+// レスポンスが返ってきたときの処理
+request.onreadystatechange = function() {
+    if(this.readyState === 4) {
+        console.log('Status:', this.status);
+        console.log('Headers:', this.getAllResponseHeaders());
+        // console.log('Body:',this.responseText);
+    }
+};
+
+// リクエストをURLに送信
+request.send();
