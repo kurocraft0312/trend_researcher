0afd95f1e63419aba27af369b519fe4a863854d7
kurocraft0312 <kurocraft0312@gmail.com>
2020-05-02 16:35:09 +0900

    json形式で吐き出し

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,16 +4,24 @@
 var request = new XMLHttpRequest();
 
 // URLを開く
-request.open('GET','https://ghapi.huchen.dev/repositories');
+request.open('GET','https://ghapi.huchen.dev/repositories',true);
 
 // レスポンスが返ってきたときの処理
-request.onreadystatechange = function() {
-    if(this.readyState === 4) {
-        console.log('Status:', this.status);
-        console.log('Headers:', this.getAllResponseHeaders());
+request.responseType = 'json';
+
+request.onload = function() {
+    var data = this.response;
+    console.log(data);
+}
+
+
+// request.onreadystatechange = function() {
+//     if(this.readyState === 4) {
+//         console.log('Status:', this.status);
+//         console.log('Headers:', this.getAllResponseHeaders());
         // console.log('Body:',this.responseText);
-    }
-};
+    // }
+// };
 
 // リクエストをURLに送信
 request.send();
